2ac8e68c57f6f5ca6578576584793e40aa12bc0d
Kirill_Bubnov <bubnov.25@mail.ru>
2019-11-26 10:18:01 +0300

    Updates

---
+++ precache-manifest.8f90c71d8b705b1123639b7ea9359d43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92ebd58f3a2d87f1b5a4",
+    "url": "/Fetch-App/static/js/runtime~main.92ebd58f.js"
+  },
+  {
+    "revision": "c1cf4f6563bdfcca55b5",
+    "url": "/Fetch-App/static/js/main.c1cf4f65.chunk.js"
+  },
+  {
+    "revision": "fb84e466eb7fa9252ab2",
+    "url": "/Fetch-App/static/js/1.fb84e466.chunk.js"
+  },
+  {
+    "revision": "c1cf4f6563bdfcca55b5",
+    "url": "/Fetch-App/static/css/main.2f7ce2ec.chunk.css"
+  },
+  {
+    "revision": "db1439cf03cd7f26f41417aa90636d32",
+    "url": "/Fetch-App/index.html"
+  }
+];
